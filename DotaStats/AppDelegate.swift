22f693376c954da2e80a68b85544ff1aa5a0ca86
Ilya Evseev <just-evseev@yandex-team.ru>
2021-12-08 12:20:51 +0300

    create project

---
+++ DotaStats/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = CostilViewController()
+        vc.view.backgroundColor = UIColor.white
+        window!.rootViewController = vc
+        window!.makeKeyAndVisible()
+        return true
+    }
+}
