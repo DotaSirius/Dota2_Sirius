23e34199f8e928ba32a3919fdc43ad81fe097d42
delet-dis <efimov.036@gmail.com>
2021-12-09 17:41:22 +0300

    ➕ added network service

---
+++ DotaStats/Data/Networking/Service/HTTPError.swift
@@ -0,0 +1,27 @@
+//
+//  HTTPError.swift
+//  DotaStats
+//
+//  Created by Igor Efimov on 09.12.2021.
+//
+
+import Foundation
+
+public enum HTTPError: String, Error {
+    case parametersNil = "Error Found : Parameters are nil."
+    case headersNil = "Error Found : Headers are Nil"
+    case encodingFailed = "Error Found : Parameter Encoding failed."
+    case decodingFailed = "Error Found : Unable to Decode the data."
+    case missingURL = "Error Found : The URL is nil."
+    case couldNotParse = "Error Found : Unable to parse the JSON response."
+    case noData = "Error Found : The data from API is Nil."
+    case FragmentResponse = "Error Found : The API's response's body has fragments."
+    case UnwrappingError = "Error Found : Unable to unwrape the data."
+    case dataTaskFailed = "Error Found : The Data Task object failed."
+    case success = "Successful Network Request"
+    case authenticationError = "Error Found : You must be Authenticated"
+    case badRequest = "Error Found : Bad Request"
+    case pageNotFound = "Error Found : Page/Route rquested not found."
+    case failed = "Error Found : Network Request failed"
+    case serverSideError = "Error Found : Server error"
+}
---
+++ DotaStats/Data/Networking/Service/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  DotaStats
+//
+//  Created by Igor Efimov on 09.12.2021.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
