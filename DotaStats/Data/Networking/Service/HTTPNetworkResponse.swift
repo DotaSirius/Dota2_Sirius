2ee6badbbb243028a1d525fd7a5e53555e1a44b5
delet-dis <efimov.036@gmail.com>
2021-12-09 17:41:22 +0300

    (#33) ➕ added network service

---
+++ DotaStats/Data/Networking/Service/HTTPNetworkResponse.swift
@@ -0,0 +1,22 @@
+//
+//  HTTPNetworkResponse.swift
+//  DotaStats
+//
+//  Created by Igor Efimov on 09.12.2021.
+//
+
+import Foundation
+
+struct HTTPNetworkResponse {
+    static func handleNetworkResponse(for response: HTTPURLResponse?) -> NetworkCommunicationResult<String>{
+        guard let res = response else { return NetworkCommunicationResult.failure(HTTPError.UnwrappingError)}
+        
+        switch res.statusCode {
+        case 200...299: return NetworkCommunicationResult.success(HTTPError.success.rawValue)
+        case 401: return NetworkCommunicationResult.failure((HTTPError.authenticationError))
+        case 400...499: return NetworkCommunicationResult.failure(HTTPError.badRequest)
+        case 500...599: return NetworkCommunicationResult.failure(HTTPError.serverSideError)
+        default: return NetworkCommunicationResult.failure(HTTPError.failed)
+        }
+    }
+}
---
+++ DotaStats/Data/Networking/Service/HTTPRequest.swift
@@ -0,0 +1,22 @@
+//
+//  HTTPRequest.swift
+//  DotaStats
+//
+//  Created by Igor Efimov on 09.12.2021.
+//
+
+import Foundation
+
+struct HTTPRequest {
+    let route: String
+    let headers: [String: String]
+    let body: Data?
+    let httpMethod: HTTPMethod
+
+    init(route: String, headers: [String: String] = [:], body: Data? = nil, httpMethod: HTTPMethod = .get) {
+        self.route = route
+        self.headers = headers
+        self.body = body
+        self.httpMethod = httpMethod
+    }
+}
