8a9ef65606481906755b52fea86051cc72febe40
delet-dis <efimov.036@gmail.com>
2021-12-10 10:59:44 +0300

    (#35) ✍🏻typo renamed

--- DotaStatsTests/Networking/NetworkServiceReceivingDataTest.swift
+++ DotaStatsTests/Networking/NetworkServiceReceivingDataTest.swift
@@ -12,36 +12,36 @@ import XCTest
 class NetworkServiceReceivingDataTest: XCTestCase {
     private func receiveData() -> [String]? {
         var receivedResult: [String]? = nil
-        
+
         let expectations = expectation(description: "\(#function)\(#line)")
-        
+
         let urlSession = URLSession(configuration: .default)
-        
+
         let networkService = NetworkService(urlSession: urlSession)
-        
+
         let constantsRequest = HTTPRequest(route: "https://api.opendota.com/api/constants")
-        
+
         networkService.processRequest(
             request: constantsRequest
         ) { (result: Result<[String], Error>) in
             switch result {
             case .success(let constants):
                 expectations.fulfill()
-                
+
                 receivedResult = constants
             case .failure:
                 break
             }
         }
-        
+
         waitForExpectations(timeout: 10, handler: .none)
-        
+
         return receivedResult
     }
-    
+
     func testArrayContainsSomeData() throws {
         let response = receiveData()
-        
+
         guard let response = response else {
             XCTAssert(response != nil)
             return
@@ -49,30 +49,30 @@ class NetworkServiceReceivingDataTest: XCTestCase {
 
         XCTAssertFalse(response.isEmpty)
     }
-    
+
     func testEveryCellOfArrayContainsSomeData() throws {
         let response = receiveData()
-        
+
         guard let response = response else {
             XCTAssert(response != nil)
             return
         }
-        
+
         response.forEach { string in
             XCTAssertFalse(string.isEmpty)
         }
     }
     
-    func testArrayContainsSomeSonstants() throws {
+    func testArrayContainsSomeConstants() throws {
         let response = receiveData()
-        
+
         guard let response = response else {
             XCTAssert(response != nil)
             return
         }
-        
+
         let arrayOfConstants = ["abilities", "region", "cluster", "countries"]
-        
+
         arrayOfConstants.forEach { constant in
             XCTAssert(response.contains(constant))
         }
