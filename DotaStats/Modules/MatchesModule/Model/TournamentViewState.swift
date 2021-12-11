aae109c07275f3b9fa9f444a1be11e7c089d3360
Solnyshkoo <kepetrova@edu.hse.ru>
2021-12-11 15:10:03 +0300

    add model(#44)

--- DotaStats/Modules/MatchesModule/Model/TournamentViewState.swift
+++ DotaStats/Modules/MatchesModule/Model/TournamentViewState.swift
@@ -2,4 +2,12 @@ import Foundation
 
 struct TournamentViewState {
     var leagueName: String
+    struct MatchViewState {
+        var radiantTeam: String
+        var radiant: Bool
+        var radiantScore: Int
+        var direScore: Int
+        var direTeam: String
+        var duration: String // "40:15"
+    }
 }
