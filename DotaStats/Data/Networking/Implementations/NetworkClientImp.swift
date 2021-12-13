38604650a1ad9ac17cbc4877354b4b632a918d7a
delet-dis <efimov.036@gmail.com>
2021-12-13 17:23:02 +0300

    ✏️formatted code

--- DotaStats/Data/Networking/Implementations/NetworkClientImp.swift
+++ DotaStats/Data/Networking/Implementations/NetworkClientImp.swift
@@ -8,78 +8,79 @@
 import Foundation
 
 struct NetworkClientImp: NetworkClient {
-	private let urlSession: URLSession
+    private let urlSession: URLSession
 
-	init(urlSession: URLSession) {
-		self.urlSession = urlSession
-	}
+    init(urlSession: URLSession) {
+        self.urlSession = urlSession
+    }
 
-	@discardableResult
-	func processRequest<T: Decodable>(
-		request: HTTPRequest,
-		completion: @escaping (Result<T, HTTPError>) -> Void
-	) -> Cancellable? {
-		do {
-			let configuredURLRequest = try configureRequest(request: request)
+    @discardableResult
+    func processRequest<T: Decodable>(
+        request: HTTPRequest,
+        completion: @escaping (Result<T, HTTPError>) -> Void
+    ) -> Cancellable? {
+        do {
+            let configuredURLRequest = try configureRequest(request: request)
 
-			let task = urlSession.dataTask(with: configuredURLRequest) { data, response, _ in
-				guard let response = response as? HTTPURLResponse, let unwrappedData = data else {
-					completion(.failure(HTTPError.decodingFailed))
-					return
-				}
-				let handledResult = HTTPNetworkResponse.handleNetworkResponse(for: response)
+            let task = urlSession.dataTask(with: configuredURLRequest) { data, response, _ in
+                guard let response = response as? HTTPURLResponse, let unwrappedData = data else {
+                    completion(.failure(HTTPError.decodingFailed))
+                    return
+                }
+                let handledResult = HTTPNetworkResponse.handleNetworkResponse(for: response)
 
-				switch handledResult {
-				case .success:
-					let jsonDecoder = JSONDecoder()
+                switch handledResult {
+                case .success:
+                    let jsonDecoder = JSONDecoder()
+                    
                     jsonDecoder.keyDecodingStrategy = request.keyDecodingStrategy
                     jsonDecoder.dateDecodingStrategy = request.dateDecodingStrategy
 
                     guard let result = try? jsonDecoder.decode(T.self, from: unwrappedData) else {
-						completion(.failure(HTTPError.decodingFailed))
-						return
-					}
+                        completion(.failure(HTTPError.decodingFailed))
+                        return
+                    }
 
-					completion(.success(result))
-				case .failure:
-					completion(.failure(HTTPError.decodingFailed))
-				}
-			}
+                    completion(.success(result))
+                case .failure:
+                    completion(.failure(HTTPError.decodingFailed))
+                }
+            }
 
-			task.resume()
-			return task
-		} catch {
+            task.resume()
+            return task
+        } catch {
             completion(.failure(HTTPError.failed))
-			return nil
-		}
-	}
+            return nil
+        }
+    }
 
-	private func configureRequest(request: HTTPRequest) throws -> URLRequest {
+    private func configureRequest(request: HTTPRequest) throws -> URLRequest {
         guard var components = URLComponents(string: request.route) else { throw HTTPError.missingURL }
-        
+
         var queriesArray = [URLQueryItem]()
-        
-        request.queryItems.forEach{ query in
+
+        request.queryItems.forEach { query in
             queriesArray.append(URLQueryItem(name: query.key, value: query.value))
         }
-        
+
         components.queryItems = queriesArray
-        
+
         components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
-        
+
         guard let componentsURL = components.url else { throw HTTPError.missingURL }
-        
+
         var generatedRequest = URLRequest(url: componentsURL)
 
-		generatedRequest.httpMethod = request.httpMethod.rawValue
-		generatedRequest.httpBody = request.body
-        
-        request.headers.forEach{
+        generatedRequest.httpMethod = request.httpMethod.rawValue
+        generatedRequest.httpBody = request.body
+
+        request.headers.forEach {
             generatedRequest.addValue($0.key, forHTTPHeaderField: $0.value)
         }
 
-		return generatedRequest
-	}
+        return generatedRequest
+    }
 }
 
 // MARK: - Cancellable
