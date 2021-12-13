3a1dbd101633dbbeb943d55a3f12224cf990b181
delet-dis <efimov.036@gmail.com>
2021-12-13 16:52:32 +0300

    ➕added PlayerSearchService

---
+++ DotaStats/Services/PlayerSearchService.swift
@@ -0,0 +1,38 @@
+//
+//  PlayerSearchService.swift
+//  DotaStats
+//
+//  Created by Igor Efimov on 13.12.2021.
+//
+
+import Foundation
+
+protocol PlayerSearchService: AnyObject {
+    func playersByName(_ name: String, closure: @escaping (Result<[Search], HTTPError>) -> Void) -> Cancellable?
+}
+
+final class PlayerSearchServiceImp: PlayerSearchService {
+    private let networkClient: NetworkClient
+
+    init(networkClient: NetworkClient) {
+        self.networkClient = networkClient
+    }
+
+    @discardableResult
+    func playersByName(_ name: String, closure: @escaping (Result<[Search], HTTPError>) -> Void) -> Cancellable? {
+        networkClient.processRequest(request: createRequest(name)) { result in
+            closure(result)
+        }
+    }
+
+    private func createRequest(_ name: String) -> HTTPRequest {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+
+        return HTTPRequest(
+            route: "https://api.opendota.com/api/search",
+            queryItems: [HTTPRequestQueryItem("q", name)],
+            dateDecodingStrategy: JSONDecoder.DateDecodingStrategy.formatted(formatter)
+        )
+    }
+}
