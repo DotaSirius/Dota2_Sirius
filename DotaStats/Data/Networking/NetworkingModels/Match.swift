498473f57019ceac25baf0b99a847f3df9683115
Борисов Матвей Евгеньевич <myborisov@stream.ru>
2021-12-13 11:55:20 +0300

    fix struct for pro match struct

---
+++ DotaStats/Data/Networking/NetworkingModels/Match.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+struct Match : Decodable {
+	let matchId : Int
+	let duration : Int
+	let startTime : Date
+	let radiantTeamId : Int?
+	let radiantName : String?
+	let direTeamId : Int?
+	let direName : String?
+	let leagueid : Int
+	let leagueName : String
+	let seriesId : Int
+	let seriesType : Int
+	let radiantScore : Int
+	let direScore : Int
+	let radiantWin : Bool
+	let radiant : Bool?
+}
