5247e936f729a29939bdeafc053478cfe30f5c7d
NastyRu <nasty.sidenko@mail.ru>
2021-12-14 17:28:28 +0300

    Add struct (#29)

--- DotaStats/Data/Networking/NetworkingModels/PlayerInfo.swift
+++ DotaStats/Data/Networking/NetworkingModels/PlayerInfo.swift
@@ -1,3 +1,63 @@
 import Foundation
 
+struct PlayerInfo {
+    let name: String
+    let leaderboardRank: Int
+    let avatar: String
+    let win: Int
+    let lose: Int
+    let recentMatches: [PlayerMatch]
+}
 
+struct PlayerMainInfo: Decodable {
+    let trackedUntil: String
+    let soloCompetitiveRank: String
+    let competitiveRank: String
+    let rankTier: Int
+    let leaderboardRank: Int
+    let mmrEstimate: MmrEstimate
+    let profile: Profile
+}
+
+struct MmrEstimate: Decodable {
+    let estimate: Int
+}
+
+struct Profile: Decodable {
+    let accountid: Int
+    let personaname: String
+    let name: String
+    let plus: Bool
+    let cheese: Int
+    let steamid: String
+    let avatar: String
+    let avatarmedium: String
+    let avatarfull: String
+    let profileurl: String
+    let lastLogin: String
+    let loccountrycode: String
+    let isContributor: Bool
+}
+
+struct PlayerWL: Decodable {
+    let win: Int
+    let lose: Int
+}
+
+struct PlayerMatch: Decodable {
+    let matchId: Int
+    let playerSlot: Int
+    let radiantWin: Bool
+    let duration: Int
+    let gameMode: Int
+    let lobbyType: Int
+    let heroId: Int
+    let startTime: Int
+    let version: Int
+    let kills: Int
+    let deaths: Int
+    let assists: Int
+    let skill: Int
+    let leaverStatus: Int
+    let partySize: Int
+}
--- DotaStats/Services/PlayerInfoService.swift
+++ DotaStats/Services/PlayerInfoService.swift
@@ -1,1 +1,25 @@
 import Foundation
+
+protocol PlayerInfoService: AnyObject {
+    func requestPlayerInfo(id: Int, completion: @escaping (Result<PlayerMainInfo, HTTPError>) -> Void) -> Cancellable?
+}
+
+final class PlayerInfoImp: PlayerInfoService {
+    private let networkClient: NetworkClient
+
+    init(networkClient: NetworkClient) {
+        self.networkClient = networkClient
+    }
+
+    @discardableResult
+    func requestPlayerInfo(id: Int, completion: @escaping (Result<PlayerMainInfo, HTTPError>) -> Void) -> Cancellable? {
+        networkClient.processRequest(
+            request: createRequest(id: id),
+            completion: completion
+        )
+    }
+
+    private func createRequest(id: Int) -> HTTPRequest {
+        HTTPRequest(route: "https://api.opendota.com/api/matches/\(id)")
+    }
+}
