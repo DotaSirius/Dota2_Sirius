d7fe44924b8cb6847af01e07c95b764f5587ceee
NastyRu <nasty.sidenko@mail.ru>
2021-12-14 12:58:48 +0300

    Update screens (#70)

--- DotaStats/AppCoordinator.swift
+++ DotaStats/AppCoordinator.swift
@@ -4,10 +4,9 @@ final class AppCoordinator {
     let tabBarController: MainTabBarController = MainTabBarController()
 
     init() {
-        let playersBuilder = playersBuilder()
-        let matchesBuilder = matchesBuilder()
-        tabBarController.setViewControllers([playersBuilder.viewControler, matchesBuilder.viewControler], animated: false)
-        tabBarController.setViewControllers(items: ["matches", "players"])
+        let playersModule = playersBuilder()
+        tabBarController.setViewControllers([playersModule.viewControler], animated: false)
+        tabBarController.setViewControllers(items: [NSLocalizedString("players", comment: "")])
     }
 }
 
@@ -18,23 +17,10 @@ extension AppCoordinator {
             networkService: NetworkServiceImp()
         )
     }
-    
-    func matchesBuilder() -> PlayersModuleBuilder { //MatchesModuleBuilder
-        PlayersModuleBuilder(
-            output: self,
-            networkService: NetworkServiceImp()
-        )
-    }
 }
 
 extension AppCoordinator: PlayersModuleOutput {
     func playersModule(_ module: PlayersModuleInput, didSelectPlayer playerId: Int) {
-        //let playerInfoBuilder = playerInfoModuleBuilder(output: self, networkService: NetworkServiceImp(), playerId: playerId)
-    }
-}
-
-extension AppCoordinator: SearchPlayerModuleOutput {
-    func searchModule(_ module: SearchPlayerModuleInput, didSelectPlayer player: Players) {
         //let playerInfoBuilder = playerInfoModuleBuilder(output: self, networkService: NetworkServiceImp(), playerId: playerId)
     }
 }
