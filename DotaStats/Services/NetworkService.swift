505132d03db942286d9c8f3c73434bea325b2047
europeec <l.europeec@ya.ru>
2021-12-14 15:22:42 +0300

    resolve conflicts

--- DotaStats/Services/NetworkService.swift
+++ DotaStats/Services/NetworkService.swift
@@ -1,9 +1,5 @@
 import Foundation
 
-protocol PlayerSearchService: AnyObject {
-    func playersByName(_ name: String, completion: @escaping (Result<[SearchPlayerResult], HTTPError>) -> Void) -> Cancellable?
-}
-
 protocol NetworkService: AnyObject {
     
 }
