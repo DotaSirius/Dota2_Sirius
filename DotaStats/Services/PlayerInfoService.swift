439f1db3155cd545d89c77747ac9ae7cf7d5c3db
NastyRu <nasty.sidenko@mail.ru>
2021-12-14 17:41:33 +0300

    Add nework service (#29)

--- DotaStats/Services/PlayerInfoService.swift
+++ DotaStats/Services/PlayerInfoService.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 protocol PlayerInfoService: AnyObject {
-    func requestPlayerInfo(id: Int, completion: @escaping (Result<PlayerMainInfo, HTTPError>) -> Void) -> Cancellable?
+    func requestPlayerMainInfo(id: Int, completion: @escaping (Result<PlayerMainInfo, HTTPError>) -> Void) -> Cancellable?
+    func requestPlayerWLInfo(id: Int, completion: @escaping (Result<PlayerWL, HTTPError>) -> Void) -> Cancellable?
+    func requestPlayerMatchesInfo(id: Int, completion: @escaping (Result<[PlayerMatch], HTTPError>) -> Void) -> Cancellable?
 }
 
 final class PlayerInfoImp: PlayerInfoService {
@@ -12,14 +14,38 @@ final class PlayerInfoImp: PlayerInfoService {
     }
 
     @discardableResult
-    func requestPlayerInfo(id: Int, completion: @escaping (Result<PlayerMainInfo, HTTPError>) -> Void) -> Cancellable? {
+    func requestPlayerMainInfo(id: Int, completion: @escaping (Result<PlayerMainInfo, HTTPError>) -> Void) -> Cancellable? {
         networkClient.processRequest(
-            request: createRequest(id: id),
+            request: createWLRequest(id: id),
+            completion: completion
+        )
+    }
+    
+    @discardableResult
+    func requestPlayerWLInfo(id: Int, completion: @escaping (Result<PlayerWL, HTTPError>) -> Void) -> Cancellable? {
+        networkClient.processRequest(
+            request: createPlayerRequest(id: id),
+            completion: completion
+        )
+    }
+    
+    @discardableResult
+    func requestPlayerMatchesInfo(id: Int, completion: @escaping (Result<[PlayerMatch], HTTPError>) -> Void) -> Cancellable? {
+        networkClient.processRequest(
+            request: createPlayerMatchesRequest(id: id),
             completion: completion
         )
     }
 
-    private func createRequest(id: Int) -> HTTPRequest {
-        HTTPRequest(route: "https://api.opendota.com/api/matches/\(id)")
+    private func createPlayerRequest(id: Int) -> HTTPRequest {
+        HTTPRequest(route: "https://api.opendota.com/api/players/\(id)")
+    }
+    
+    private func createWLRequest(id: Int) -> HTTPRequest {
+        HTTPRequest(route: "https://api.opendota.com/api/players/\(id)/wl")
+    }
+    
+    private func createPlayerMatchesRequest(id: Int) -> HTTPRequest {
+        HTTPRequest(route: "https://api.opendota.com/api/players/\(id)/recentMatches")
     }
 }
