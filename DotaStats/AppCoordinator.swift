4f4b908da32e956880388213e4f6384572341c1d
NastyRu <nasty.sidenko@mail.ru>
2021-12-14 15:07:35 +0300

    Add module to coordinator (#2)

--- DotaStats/AppCoordinator.swift
+++ DotaStats/AppCoordinator.swift
@@ -1,12 +1,45 @@
 import Foundation
 
 final class AppCoordinator {
+    let tabBarController: MainTabBarController = MainTabBarController()
+
+    init() {
+        let playersModule = playersBuilder()
+        let matchesModule = matchesBuilder()
+        tabBarController.setViewControllers([playersModule.viewControler, matchesModule.viewControler], animated: false)
+        tabBarController.setViewControllers(items: [NSLocalizedString("players", comment: ""),
+                                                    NSLocalizedString("matches", comment: "")])
+    }
+}
+
+extension AppCoordinator {
+    private func playersBuilder() -> PlayersModuleBuilder {
+        PlayersModuleBuilder(
+            output: self,
+            networkService: NetworkServiceImp()
+        )
+    }
+    
+    private func matchesBuilder() -> MatchesModuleBuilder {
+        MatchesModuleBuilder(
+            output: self,
+            matchesService: MatchesServiceImp(
+                networkClient: NetworkClientImp(
+                    urlSession: URLSession(configuration: .default)
+                )
+            )
+        )
+    }
+}
+
+extension AppCoordinator: PlayersModuleOutput {
+    func playersModule(_ module: PlayersModuleInput, didSelectPlayer playerId: Int) {
+        //let playerInfoBuilder = playerInfoModuleBuilder(output: self, networkService: NetworkServiceImp(), playerId: playerId)
+    }
 }
 
 extension AppCoordinator: MatchesModuleOutput {
-    func matchesModule(_ module: MatchesModuleInput, didSelectPlayer matchId: Int) {
+    func matchesModule(_ module: MatchesModuleInput, didSelectMatch matchId: Int) {
         // todo
     }
-    
-    
 }
--- DotaStats/MainTabBarController.swift
+++ DotaStats/MainTabBarController.swift
@@ -6,21 +6,10 @@ final class MainTabBarController: UITabBarController {
         tabBar.backgroundColor = ColorPalette.alternatеBackground
         tabBar.unselectedItemTintColor = ColorPalette.text
         tabBar.tintColor = ColorPalette.accent
-        let module = MatchesModuleBuilder(
-            output: AppCoordinator(),
-            matchesService: MatchesServiceImp(
-                networkClient: NetworkClientImp(
-                    urlSession: URLSession(configuration: .default)
-                )
-            )
-        )
-        let viewControllers = [
-            // TODO: replace with Matches and Players viewControllers.
-            module.viewControler,
-            CostilViewController()
-        ]
-        setViewControllers(viewControllers, animated: false)
-        let items = ["matches", "players"]
+    }
+    
+    func setViewControllers(items: [String]) {
+        guard let viewControllers = viewControllers else { return }
         for i in 0 ..< viewControllers.count {
             viewControllers[i].view.backgroundColor = ColorPalette.mainBackground
             tabBar.items![i].image = UIImage(named: items[i])
