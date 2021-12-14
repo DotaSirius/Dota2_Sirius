f816518bfae7902e84ec35176aec7b437c851923
Борисов Матвей Евгеньевич <myborisov@stream.ru>
2021-12-14 16:09:03 +0300

    #68 add base cached image view

--- DotaStats/Common/CachedImageView/CachedImageView.swift
+++ DotaStats/Common/CachedImageView/CachedImageView.swift
@@ -1,18 +1,42 @@
-//
-//  CachedImageView.swift
-//  DotaStats
-//
-//  Created by Борисов Матвей Евгеньевич on 13.12.2021.
-//
-
 import UIKit
 
-class CachedImageView: UIView {
-
+class CachedImageView: UIImageView {
     private let imageLoader = ImageServiceImp()
+    private let activity = UIActivityIndicatorView()
 
 
-    func loadWithUrl(_ url: String, completion: @escaping (Result<[Players], HTTPError>) -> Void) {
 
+    init() {
+        super.init(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+        loaderLayout()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func loadWithUrl(_ url: String) {
+        activity.startAnimating()
+
+        imageLoader.loadWithUrl(url) { result in
+            switch result {
+            case .success(let newImage):
+                self.image = newImage
+            case .failure:
+                break
+            }
+            self.activity.stopAnimating()
+        }
+    }
+
+    private func loaderLayout() {
+        backgroundColor = .white
+        addSubview(activity)
+        activity.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activity.centerXAnchor.constraint(equalTo: centerXAnchor),
+            activity.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
     }
 }
