813afe4c72d5d252ef5f28097e09c2605cd66976
Solnyshkoo <kepetrova@edu.hse.ru>
2021-12-16 15:01:07 +0300

    add files

--- DotaStats/AppDelegate.swift
+++ DotaStats/AppDelegate.swift
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         let networkServise = NetworkClientImp(urlSession: .init(configuration: .default))
         let teamInfo = TeamInfoImp(networkClient: networkServise)
-        teamInfo.requestTeamMainInfo(id: 1) { result in
+        teamInfo.requestTeamMainInfo(id: 15) { result in
             switch result {
             case .success(let data):
                 print(data)
