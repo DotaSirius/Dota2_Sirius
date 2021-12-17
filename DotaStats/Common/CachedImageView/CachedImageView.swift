c98b5412a7c4645b50400c0f944e2500725df38c
Борисов Матвей Евгеньевич <myborisov@stream.ru>
2021-12-17 16:37:46 +0300

    #114 fix

--- DotaStats/Common/CachedImageView/CachedImageView.swift
+++ DotaStats/Common/CachedImageView/CachedImageView.swift
@@ -31,6 +31,7 @@ final class CachedImageView: UIImageView {
     }
 
     override func layoutSubviews() {
+        super.layoutSubviews()
         gradientLayer.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
     }
 
